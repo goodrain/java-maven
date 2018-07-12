6601c8286b7284cee499d92466fd18836f648814
goodrain <thor@goodrain.com>
2018-07-12 10:47:47 +0800

    update date

--- src/main/java/com/goodrain/demo/web/HelloController.java
+++ src/main/java/com/goodrain/demo/web/HelloController.java
@@ -4,6 +4,8 @@ import com.goodrain.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 @RestController
 public class HelloController {
@@ -14,6 +16,7 @@ public class HelloController {
     @RequestMapping("/")
     public String home() {
         helloService.say();
-        return "Hello World! 你好，世界!!!  2018/03";
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "Hello World! 你好，世界!!! "+df.format(new Date());
     }
 }
